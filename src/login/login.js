a503b031208d24d09500daedf8331b445b38feb8
Jean-Loup Trolliard <jl.trolliard@gmail.com>
2017-11-10 20:26:27 +0900

    data test

--- src/login/login.js
+++ src/login/login.js
@@ -1,19 +1,8 @@
 import './login.css'
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
+import json from './../../public/stations.json'
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyCGAmwgYWVAqYE05eVBymPULVbqfV71gjY',
-  authDomain: 'skybikes-5dc1d.firebaseapp.com',
-  databaseURL: 'https://skybikes-5dc1d.firebaseio.com',
-  projectId: 'skybikes-5dc1d',
-  storageBucket: 'skybikes-5dc1d.appspot.com',
-  messagingSenderId: '967665026789'
-})
-
-const getEnvironment = () => firebase.database().ref('/env').once('value').then((snap) => console.log(snap.val()))
-getEnvironment()
+console.log(json)
+json.map((e) => console.log(e))
 
 export const isAdmin = (id) => id === 'admin'
 
